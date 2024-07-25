cdb3e78f9bf907f5d29ebfd499ddc14a6fb0533f
robsonbucci <robsonbucci@hotmail.com>
2024-07-25 11:00:37 -0300

    configurando projeto

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <button class="btn btn-primary" @click="count++">{{ count }}</button>
+</template>
+
+<style></style>
