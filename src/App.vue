f577a80e24e163f1a026dcb5f01b977fa4c5ac2f
robsonbucci <robsonbucci@hotmail.com>
2024-07-25 16:08:16 -0300

    criando grid

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,68 @@
-<script setup>
-import { ref } from 'vue';
-
-const count = ref(0);
-</script>
+<script setup></script>
 
 <template>
-  <button class="btn btn-primary" @click="count++">{{ count }}</button>
+  <nav class="navbar navbar-expand-lg text-light navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/188/188987.png"
+          alt="logo"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-center text-decoration-none"
+        />
+        PokeVite
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- CONTENT -->
+  <div class="main">
+    <router-view></router-view>
+  </div>
+
+  <footer class="bg-primary text-light">
+    <div>
+      Updated
+      {{
+        `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+          new Date(),
+        )} ${new Date().getFullYear()}`
+      }}
+    </div>
+  </footer>
 </template>
 
-<style></style>
+<style>
+footer {
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+}
+</style>
---
+++ src/views/HomeViews.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="container mt-4 p-auto">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem">
+            <img
+              src="https://www.ensino.eu/media/3kyh4vko/pokemon.jpg?width=300&height=300"
+              class="card-img-top"
+              alt="card"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the
+                card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the
+                card's content.
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
