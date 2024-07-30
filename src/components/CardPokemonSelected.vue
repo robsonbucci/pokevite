28135e2efd8d7b9c3fc957997061a6e6a7822284
robsonbucci <robsonbucci@hotmail.com>
2024-07-30 17:55:02 -0300

    evento de click para interagir com itens

---
+++ src/components/CardPokemonSelected.vue
@@ -0,0 +1,52 @@
+<script setup>
+const pokemon = defineProps([
+  "urlBaseSVG",
+  "name",
+  "xp",
+  "height",
+  "image",
+  "loading",
+]);
+</script>
+
+<template>
+  <div class="card cardPokemonSelected">
+    <img
+      :src="
+        image ||
+        'https://raw.githubusercontent.com/yurimarcon/PokeVite/305b2449732026a2f631cbd5908fd2e178b2aa7e/src/assets/egg_pokemon.svg'
+      "
+      :alt="name"
+      class="card-img-top p-2"
+      :class="!loading ? 'animate__animated animate__jackInTheBox' : ''"
+      height="250"
+    />
+    <div class="card-body">
+      <h5 class="card-title text-center">{{ name }}</h5>
+      <hr />
+      <div class="row">
+        <section class="col">
+          <strong>XP: </strong>
+          <span>{{ xp || 0 }}</span>
+        </section>
+        <section class="col">
+          <strong>Altura: </strong>
+          <span>{{ height || 0 }}</span>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cardPokemonSelected {
+  height: 450px;
+  background: rgb(72, 63, 251);
+  background: radial-gradient(
+    circle,
+    rgb(255, 123, 0) 0%,
+    rgb(196, 209, 12) 100%
+  );
+  cursor: pointer;
+}
+</style>
--- src/views/HomeViews.vue
+++ src/views/HomeViews.vue
@@ -2,76 +2,115 @@
 import { computed, onMounted, reactive, ref } from "vue";
 import LIstPokemons from "../components/LIstPokemons.vue";
 import ListPokemonsSkeleton from "../components/ListPokemonsSkeleton.vue";
+import CardPokemonSelected from "../components/CardPokemonSelected.vue";
 
 let pokemons = reactive([]);
 let searchPokemonField = ref("");
+let pokemonSelected = reactive(ref([]));
+let loading = ref(false);
+
 const urlBaseSVG = ref(
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
 );
 
 onMounted(async () => {
-    const lista = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
-    );
-    const data = await lista.json();
-    pokemons.push(...data.results);
+  const lista = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+  );
+  const data = await lista.json();
+  pokemons.push(...data.results);
 });
 
 const pokemonsFiltered = computed(() => {
-    if (pokemons?.length && searchPokemonField.value) {
-        return pokemons.filter((pokemon) =>
-            pokemon.name.includes(searchPokemonField.value.toLocaleLowerCase())
-        );
-    }
-    return pokemons;
+  if (pokemons?.length && searchPokemonField.value) {
+    return pokemons.filter((pokemon) =>
+      pokemon.name.includes(searchPokemonField.value.toLocaleLowerCase())
+    );
+  }
+  return pokemons;
 });
+
+const selectPokemon = async (pokemon) => {
+  loading.value = true;
+  // fetch(pokemon.url)
+  //   .then((res) => res.json())
+  //   .then((pokemon) => (pokemonSelected.value = pokemon))
+  //   .catch((err) => alert(err))
+  //   .finally((pokemonSelected.value = false));
+  try {
+    const data = await fetch(pokemon.url);
+    const result = await data.json();
+    pokemonSelected.value = result;
+    loading.value = false;
+    return pokemonSelected;
+  } catch (err) {
+    console.error(err);
+  }
+};
 </script>
 
 <template>
-    <main>
-        <div class="container mt-4">
-            <div class="row">
-                <div class="col-sm-12 col-md-7">
-                    <div class="card">
-                        <div v-if="!pokemons.length">
-                            <div class="card-body row">
-                                <ListPokemonsSkeleton v-for="i in 10" />
-                            </div>
-                        </div>
-
-                        <div v-else>
-                            <div class="card-body row">
-                                <div class="mb-3">
-                                    <label
-                                        hidden
-                                        for="searchPokemonField"
-                                        class="form-label"
-                                        >Pesquisar...</label
-                                    >
-                                    <input
-                                        v-model="searchPokemonField"
-                                        type="text"
-                                        class="form-control"
-                                        id="searchPokemonField"
-                                        placeholder="Pesquisar..."
-                                    />
-                                </div>
-
-                                <LIstPokemons
-                                    v-for="pokemon in pokemonsFiltered"
-                                    :key="pokemon.url"
-                                    :name="pokemon.name"
-                                    :urlBaseSVG="
-                                        urlBaseSVG +
-                                        pokemon.url.split('/').slice(-2)[0] +
-                                        '.svg'
-                                    "
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <main>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <CardPokemonSelected
+            :urlBaseSVG="urlBaseSVG"
+            :name="pokemonSelected?.name"
+            :xp="pokemonSelected?.base_experience"
+            :height="pokemonSelected?.height"
+            :image="pokemonSelected?.sprites?.other?.dream_world?.front_default"
+            :loading="loading"
+          />
         </div>
-    </main>
+        <div class="col-sm-12 col-md-6">
+          <!-- SKELETON -->
+          <div class="card card-list">
+            <div v-if="!pokemons.length">
+              <div class="card-body row">
+                <ListPokemonsSkeleton v-for="i in 10" />
+              </div>
+            </div>
+            <!-- SKELETON - FIM -->
+
+            <div v-else>
+              <div class="col-sm-12 col-md-7"></div>
+              <div class="card-body row">
+                <div class="mb-3">
+                  <label hidden for="searchPokemonField" class="form-label"
+                    >Pesquisar...</label
+                  >
+                  <input
+                    v-model="searchPokemonField"
+                    type="text"
+                    class="form-control"
+                    id="searchPokemonField"
+                    placeholder="Pesquisar..."
+                  />
+                </div>
+
+                <LIstPokemons
+                  v-for="pokemon in pokemonsFiltered"
+                  :key="pokemon.url"
+                  :name="pokemon.name"
+                  :urlBaseSVG="
+                    urlBaseSVG + pokemon.url.split('/').slice(-2)[0] + '.svg'
+                  "
+                  @click="selectPokemon(pokemon)"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.card-list {
+  max-height: 450px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+</style>
